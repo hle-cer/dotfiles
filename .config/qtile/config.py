1f2479a366ca5c7d62ba9098444073067d725fc5
hle-cer <vid@tuta.com>
2024-02-07 23:46:54 +0100

    Added qtile config

---
+++ .config/qtile/config.py
@@ -0,0 +1,214 @@
+
+#     config.py 
+
+# All the needed python libraies
+import os
+import subprocess
+import dbus_next
+from keybindings import keys
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from qtile_extras import widget
+from qtile_extras.widget.decorations import RectDecoration
+
+#colors
+main = '#7aa2f7'
+sec = '#f7768e'
+forg = '#9aa5ce'
+bg = '#1a1b26'
+
+decoration_group = {
+    "decorations": [
+        RectDecoration(colour=bg, radius=10, filled=True, padding_y=0, group=True)
+    ],
+    "padding": 10,
+}
+
+# Variables
+mod = "mod4"
+terminal = "alacritty"
+browser = "brave"
+
+groups = [
+    Group('a', label = ""),
+    Group('s', label = ""),
+    Group('d', label = ""),
+    Group('f', label = "󰙯"),
+    Group('u', label = ""),
+    Group('i', label = ""),
+    Group('o', label = ""),
+    Group('p', label = ""),
+    ]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+layouts = [
+    layout.Columns(
+            border_width=2,
+            font = "JetBrainsMono Nerd Font",
+            margin = 3,
+            border_focus = main,
+            border_normal = '494949',
+            border_on_single=True,
+            ),
+
+    layout.Max(),
+    ]
+widget_defaults = dict(
+    font="JetBrainsMono Nerd Font",
+    fontsize=14,
+    padding=3,
+    foreground=forg
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(        bottom=bar.Gap(3),
+                   left=bar.Gap(3),
+                   right=bar.Gap(3),
+        top=bar.Bar(
+[
+
+                            ## Widgets ##
+
+
+                #-- Workspaces -------------------------#
+                widget.GroupBox(
+                    highlight_method='text',
+                    rounded = False,
+                    disable_drag = True,
+                    active = "#979797",
+                    inactive="#494949",
+                    this_current_screen_border=main,
+                    margin_y = 3,
+                    margin_x = 0,
+                    padding_y = 0,
+                    fontsize = 20,
+                    font = "JetBrainsMono Nerd Font",
+                    **decoration_group
+                    ),
+                #---------------------------------------#
+
+                widget.Spacer(10),
+    
+                #-- System tray ------------------------#
+                widget.Systray(),
+                #---------------------------------------#
+    
+                widget.Spacer(),
+
+                #-- Clock ------------------------------#
+                widget.Clock(
+                    format=" %H:%M  %b %d",
+                    **decoration_group
+                    ),
+                #---------------------------------------#
+                
+                widget.Spacer(),
+                
+                #-- Connection info --------------------#
+                widget.Wlan(
+                    format='  {essid}',
+                    **decoration_group
+                    ),
+                #---------------------------------------#
+        
+                widget.Spacer(5),
+
+                #-- Volume -----------------------------#
+                widget.Volume(
+                    fmt='󰕾 {}',
+                    **decoration_group
+                    ),
+                #---------------------------------------#
+    
+                widget.Spacer(5),
+
+                #-- Battery [install upower] -----------#
+                widget.Spacer(
+                    10,
+                    **decoration_group
+                     ),
+                widget.UPowerWidget(
+                    border_colour = forg,
+                    border_critical_colour = '#f7768e',
+                    border_charge_colour = forg,
+                    fill_normal = forg,
+                    fill_critical = forg,
+                    fill_low = forg,
+                    text_charging = '({percentage:.0f}%)󰚥 ',
+                    text_discharging = '{percentage:.0f}% ',
+                    **decoration_group
+                    ),
+                widget.Spacer(
+                    10,
+                    **decoration_group
+                    ),
+                #---------------------------------------#
+            ],
+            28,
+            background = '#00000000',
+            margin = [6,6,3,6],
+
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+
+            font = "JetBrainsMono Nerd Font",
+            border_focus = main,
+            border_normal = '494949',
+            border_width=2,
+
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+auto_minimize = True
+wmname = "qtile"
+
+    ## autostart ##
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.Popen([home])
---
+++ .config/qtile/keybindings.py
@@ -0,0 +1,84 @@
+# Imports
+import os
+import subprocess
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = "alacritty"
+browser = "brave"
+
+
+keys = [
+
+    # Rofi
+    Key([mod], "m", lazy.spawn(os.path.expanduser("rofi -show drun")), desc="rofi"),
+    Key([mod, "control"], "m", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/powermenu")), desc="rofi power menu"),
+    Key([mod, "shift"], "m", lazy.spawn(os.path.expanduser("rofi -show calc")), desc="rofi calc"),
+    Key(["mod1"], "Tab", lazy.spawn(os.path.expanduser("rofi -show window")), desc="rofi window"),
+
+    # Player
+    Key([mod], "down", lazy.spawn(os.path.expanduser("playerctl play-pause")), desc="play/pause"),
+    Key([mod], "right", lazy.spawn(os.path.expanduser("playerctl next")), desc="next"),
+    Key([mod], "left", lazy.spawn(os.path.expanduser("playerctl previous")), desc="prev"),
+    Key([mod], "up", lazy.spawn(os.path.expanduser("playerctl open")), desc="open"),
+    
+
+    # Volume
+    Key([], "XF86AudioMicMute", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/volume.sh mic")), desc="mute/unmute mic"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/volume.sh up")), desc="vol up"),
+    Key([], "XF86AudioLowerVolume", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/volume.sh down")), desc="vol down"),
+    Key([], "XF86AudioMute", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/volume.sh mute")), desc="mute/unmute"),
+
+    # Brightness
+    Key([], "XF86MonBrightnessUp", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/brightness.sh up")), desc="Brightness up"),
+    Key([], "XF86MonBrightnessDown", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/brightness.sh down")), desc="Brightness down"),
+
+    # Scrot
+    Key([], "Print", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/screenshot.sh ")), desc="normal screenshot"),
+    Key(["shift"], "Print", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/screenshot.sh select")), desc="selection screenshot"),
+    Key(["control"], "Print", lazy.spawn(os.path.expanduser("~/.config/qtile/scripts/screenshot.sh window")), desc="window screenshot"),
+
+    # Hotspot
+
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
+    # Move windows between windows
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+
+    # Toggle between split and unsplit sides of stack
+    # Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
+
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "w", lazy.spawn(browser), desc="Launch browser"),
+
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+
+    # Layouts
+    Key([mod], "1", lazy.window.toggle_floating(), desc="Toggle floating layout"),
+    Key([mod], "2", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen layout"),
+    Key([mod], "3", lazy.window.toggle_minimize(), desc="Toggle minimization on focused window"),
+]
